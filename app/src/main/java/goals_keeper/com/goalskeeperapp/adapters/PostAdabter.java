20e243e774522ef956632e14aca34f4e9829a40b
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-28 23:02:38 +0200

    Modifies post_fragment Adds recycler view for goals selection Adds adabter for recycler view Adds plain text view

---
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/adapters/PostAdabter.java
@@ -0,0 +1,59 @@
+package goals_keeper.com.goalskeeperapp.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import goals_keeper.com.goalskeeperapp.R;
+
+/**
+ * Created by hamamsy on 28/11/15.
+ */
+public class PostAdabter extends RecyclerView.Adapter<PostAdabter.ViewHolder> {
+    private String[] mDataset; //dummy data for now
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout mLinearLayout;
+
+        public ViewHolder(LinearLayout v) {
+            super(v);
+            mLinearLayout = v;
+        }
+    }
+
+    public PostAdabter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public PostAdabter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_goal_selection, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder((LinearLayout)v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        TextView tv =(TextView) holder.mLinearLayout.findViewById(R.id.my_text_view);
+        tv.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/PostFragment.java
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/PostFragment.java
@@ -2,21 +2,44 @@ package goals_keeper.com.goalskeeperapp.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import goals_keeper.com.goalskeeperapp.R;
+import goals_keeper.com.goalskeeperapp.adapters.PostAdabter;
 
 /**
  * Created by hamamsy on 28/11/15.
  */
 public class PostFragment extends android.support.v4.app.Fragment {
+    @Bind(R.id.my_recycler_view)
+    RecyclerView mRecyclerView;
+
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, container, false); // Inflating the fragment layout
+
+        ButterKnife.bind(this, view);
+
+        // use a linear layout manager
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new PostAdabter(new String[]{"Eat Betngan","Play BetnganBall"});
+        mRecyclerView.setAdapter(mAdapter);
         return view;
     }
 }
