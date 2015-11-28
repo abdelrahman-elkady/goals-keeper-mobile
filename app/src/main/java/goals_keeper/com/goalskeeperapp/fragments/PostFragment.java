bc97446c48743ad3e25f8b357f265392d58f4760
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-28 13:52:31 +0200

    Adds listener to btn_add_post

---
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/PostFragment.java
@@ -0,0 +1,7 @@
+package goals_keeper.com.goalskeeperapp.fragments;
+
+/**
+ * Created by hamamsy on 28/11/15.
+ */
+public class PostFragment {
+}
--- app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/TimelineFragment.java
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/TimelineFragment.java
@@ -3,10 +3,15 @@ package goals_keeper.com.goalskeeperapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import goals_keeper.com.goalskeeperapp.R;
 
 /**
@@ -15,10 +20,26 @@ import goals_keeper.com.goalskeeperapp.R;
  * @author kady
  */
 public class TimelineFragment extends Fragment {
+    @Bind(R.id.btn_add_post)
+    Button mAddPostButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timeline, container, false);
+
+        ButterKnife.bind(this, view);
+        mAddPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                PostFragment postFragment = new PostFragment();
+                fragmentTransaction.replace(R.id.main_fragment_container, postFragment);
+                fragmentTransaction.commit();
+            }
+        });
         return view;
     }
 }
