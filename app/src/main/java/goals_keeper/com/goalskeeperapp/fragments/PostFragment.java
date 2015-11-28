60c86f86888ad0eea54483af43e8f366e4fb7fd5
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-28 13:53:38 +0200

    Adds new fragment for post

--- app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/PostFragment.java
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/PostFragment.java
@@ -1,7 +1,26 @@
 package goals_keeper.com.goalskeeperapp.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import goals_keeper.com.goalskeeperapp.R;
+
 /**
  * Created by hamamsy on 28/11/15.
  */
-public class PostFragment {
+public class PostFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true); // To support modifying the toolbar menu on fragment change
+
+        final View view = inflater.inflate(R.layout.fragment_post, container, false); // Inflating the fragment layout
+        return view;
+    }
 }
