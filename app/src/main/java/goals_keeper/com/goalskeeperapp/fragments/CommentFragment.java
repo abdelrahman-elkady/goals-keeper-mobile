9a99d9722f45b91d8b8790c28692a370e8e0f98b
Abdelrahman <a.mahmoud1994@gmail.com>
2015-12-01 22:24:36 +0200

    Adding the onCreateView method to the comment fragment

--- app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/CommentFragment.java
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/CommentFragment.java
@@ -1,7 +1,23 @@
 package goals_keeper.com.goalskeeperapp.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import goals_keeper.com.goalskeeperapp.R;
+
 /**
  * Created by abdelrahman on 01/12/15.
  */
-public class CommentFragment {
+public class CommentFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_comment, container, false);
+        return view;
+    }
 }
