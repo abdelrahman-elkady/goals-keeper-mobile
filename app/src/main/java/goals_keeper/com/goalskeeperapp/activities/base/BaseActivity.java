50b4a1d61650147f44003589cb26a6b6ba041507
kady <elkadyabdelrahman@gmail.com>
2015-12-06 21:55:25 +0200

    Moving base activity to base package

---
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/activities/base/BaseActivity.java
@@ -0,0 +1,47 @@
+package goals_keeper.com.goalskeeperapp.activities.base;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import goals_keeper.com.goalskeeperapp.R;
+import goals_keeper.com.goalskeeperapp.activities.AboutActivity;
+
+/**
+ * Created by kady on 05/12/15.
+ *
+ * @author kady
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_default, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0) {
+            this.finish();
+        } else {
+            getSupportFragmentManager().popBackStack();
+        }
+    }
+}
