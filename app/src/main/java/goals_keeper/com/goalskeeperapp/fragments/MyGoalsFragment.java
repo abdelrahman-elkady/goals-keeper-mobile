20b7a3db0627d9f0aa9230617b4a5b28e301ce42
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-16 05:06:16 +0200

    Modfifies call back for user goals

--- app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/MyGoalsFragment.java
+++ app/src/main/java/goals_keeper/com/goalskeeperapp/fragments/MyGoalsFragment.java
@@ -1,14 +1,18 @@
 package goals_keeper.com.goalskeeperapp.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,8 +22,12 @@ import butterknife.OnClick;
 import goals_keeper.com.goalskeeperapp.R;
 import goals_keeper.com.goalskeeperapp.activities.SearchActivity;
 import goals_keeper.com.goalskeeperapp.adapters.MyGoalsAdapter;
+import goals_keeper.com.goalskeeperapp.api.Api;
 import goals_keeper.com.goalskeeperapp.models.Goal;
 import goals_keeper.com.goalskeeperapp.utils.Constants;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 /**
  * Created by kady on 25/11/15.
@@ -35,6 +43,7 @@ public class MyGoalsFragment extends android.support.v4.app.Fragment {
 
     MyGoalsAdapter mGoalsAdapter;
     ArrayList<Goal> mData;
+    SharedPreferences mSharedPreferences;
 
     @Nullable
     @Override
@@ -42,6 +51,8 @@ public class MyGoalsFragment extends android.support.v4.app.Fragment {
         final View view = inflater.inflate(R.layout.fragment_goals_preview, container, false);
 
         ButterKnife.bind(this, view);
+
+        mSharedPreferences = getActivity().getSharedPreferences(Constants.SHARED_PREFS_KEY, Context.MODE_PRIVATE);
         initData();
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -55,7 +66,22 @@ public class MyGoalsFragment extends android.support.v4.app.Fragment {
     }
 
     private void initData() {
-        mData = new ArrayList<>();
+        mData = new ArrayList<Goal>();
+        int userId = mSharedPreferences.getInt(Constants.USER_ID, -1);
+        Api.privateRoutes(getActivity()).getUserGoals(userId).enqueue(new Callback<ArrayList<Goal>>() {
+            @Override
+            public void onResponse(Response<ArrayList<Goal>> response, Retrofit retrofit) {
+                mData = response.body();
+                Toast.makeText(getActivity(), "Succeeded to retrieve goals list", Toast.LENGTH_SHORT).show();
+                Log.d("GOALS LIST", response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(getActivity(), "Failed to retrieve goals list", Toast.LENGTH_SHORT).show();
+                Log.e("GOALS LIST", t.getMessage());
+            }
+        });
 
     }
 
